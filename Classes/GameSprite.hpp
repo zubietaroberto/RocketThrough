b4593be82f38529920ca732493092736bc5d148b
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2016-04-06 17:53:02 -0500

    Successful windows compilation

--- Classes/GameSprite.hpp
+++ Classes/GameSprite.hpp
@@ -1,1 +1,14 @@
-#ifndef __GAMESPRITE_HPP__#define __GAMESPRITE_HPP__#include "cocos2d.h"USING_NS_CC;class GameSprite : public Sprite {public:  CC_SYNTHESIZE(float, _radius, Radius);	static GameSprite* createWithFrameName(const char * pszSpriteFrameName);};#endif // __GAMESPRITE_HPP__
+#pragma once
+
+#include "cocos2d.h"
+
+USING_NS_CC;
+
+class GameSprite : public Sprite {
+public:
+
+	CC_SYNTHESIZE(float, _radius, Radius);
+	static GameSprite* createWithFrameName(const char * pszSpriteFrameName);
+
+};
+
--- Classes/LineContainer.cpp
+++ Classes/LineContainer.cpp
@@ -1,1 +1,49 @@
-#include "LineContainer.hpp"USING_NS_CC;LineContainer::LineContainer() {	_dash = 10;	_dashSpace = 10;	_lineType = LINE_NONE;	_screenSize = Director::getInstance()->getWinSize();	_energyLineX = _screenSize.width * 0.96f;	_energyHeight = _screenSize.height * 0.8f;	glLineWidth(8.0 * CC_CONTENT_SCALE_FACTOR());	this->reset();}LineContainer* LineContainer::create () {	LineContainer * node = new LineContainer();	if (node) {		node->autorelease();		return node;	}	CC_SAFE_DELETE(node);	return NULL;}void LineContainer::reset() {    _energy = 1.0;    _energyDecrement = 0.015f;}void LineContainer::update (float dt) {    _energy -= dt * _energyDecrement;    if (_energy < 0) _energy = 0;}void LineContainer::setEnergyDecrement(float value) {    _energyDecrement += value;    if (_energyDecrement > 0.07) _energyDecrement = 0.07;}void LineContainer::draw (Renderer *renderer, const Mat4& transform, uint32_t flags) {}
+#include "LineContainer.hpp"
+
+#include "LineContainer.hpp"
+
+USING_NS_CC;
+
+LineContainer::LineContainer() {
+
+	_dash = 10;
+	_dashSpace = 10;
+	_lineType = LINE_NONE;
+	_screenSize = Director::getInstance()->getWinSize();
+	_energyLineX = _screenSize.width * 0.96f;
+	_energyHeight = _screenSize.height * 0.8f;
+
+	glLineWidth(8.0 * CC_CONTENT_SCALE_FACTOR());
+
+	this->reset();
+}
+
+LineContainer* LineContainer::create() {
+	LineContainer * node = new LineContainer();
+	if (node) {
+		node->autorelease();
+		return node;
+	}
+	CC_SAFE_DELETE(node);
+	return NULL;
+}
+
+void LineContainer::reset() {
+	_energy = 1.0;
+	_energyDecrement = 0.015f;
+}
+
+void LineContainer::update(float dt) {
+	_energy -= dt * _energyDecrement;
+	if (_energy < 0) _energy = 0;
+}
+
+void LineContainer::setEnergyDecrement(float value) {
+	_energyDecrement += value;
+	if (_energyDecrement > 0.07) _energyDecrement = 0.07;
+}
+
+void LineContainer::draw(Renderer *renderer, const Mat4& transform, uint32_t flags) {
+
+
+}
